ee82e0d52ae02d6d1a8b02eb2e710359bdb9f510
Matyas Forian Szabo <forianszm@gmail.com>
2020-09-17 09:07:38 +0200

    organize examples a bit more

--- Snake-Game/Program.cs
+++ Snake-Game/Program.cs
@@ -1,6 +1,4 @@
 ﻿using SixLabors.ImageSharp;
-using SixLabors.ImageSharp.Drawing.Processing;
-using SixLabors.ImageSharp.Processing;
 
 namespace Snake_Game
 {
@@ -44,28 +42,26 @@ namespace Snake_Game
         {
             PerlinApp.Stage.BackgroundColor = new Rgb24(200, 200, 200);
             //TextFieldTests();
+            //TestDragging();
+            //TestTextureSubRegion();
+            //TestTransparency();
             
+            // sample snake game
             var scoreTextField = new TextField(PerlinApp.FontRobotoMono.CreateFont(18));
             scoreTextField.BackgroundColor = Color.Coral;
             scoreTextField.X = 260;
             scoreTextField.Y = 5;
             scoreTextField.FontColor = Color.White;
             PerlinApp.Stage.AddChild(scoreTextField);
-
-            // sample snake game
             var highScore = 0;
             var worldSize = new Vector2(Width, Height);
             var world = new World(worldSize, _cellSize);
             var snake = new Snake(world);
-            
-            PerlinApp.ShowStats(HorizontalAlignment.Right, VerticalAlignment.Bottom, 2);
-
-            
             snake.ScoreChanged += () => scoreTextField.Text = snake.Score.ToString();
             snake.ScoreChanged += () => highScore = Math.Max(highScore, snake.Score);
             var headPath = Path.Combine(AppContext.BaseDirectory, "Assets", "snake-head.png");
             var bodyPath = Path.Combine(AppContext.BaseDirectory, "Assets", "snake-3.png");
-            //            var tf = new TextField(PerlinApp.FontRobotoMono.CreateFont(14)) {Text = "BBA", Width = 62, Height = 32, X = 40, Y = 20};
+
             var s2 = new Sprite(headPath) { X = 150, Y = 150 };
             var s3 = new Sprite(headPath) { X = 0, Y = 50 };
             var s4 = new Sprite(bodyPath) { X = 0, Y = 30 };
@@ -73,56 +69,23 @@ namespace Snake_Game
             s2.PivotX = 8;
             s2.PivotY = 8;
             s3.Rotation = 6f;
-
-            //s3.ScaleX = 2;
-            var tf2 = new TextField(PerlinApp.FontRobotoMono.CreateFont(13))
-            {
-                Text = "ZddedZBféáŰ",
-                Width = 62,
-                Height = 56,
-                X = 190,
-                Y = 20,
-                //  AutoSize = true,
-                BackgroundColor = new Rgba32(255, 0, 0, 200)
-            };
-            PerlinApp.Stage.AddChild(tf2);
-            
             PerlinApp.Stage.AddChild(s2);
             s2.AddChild(s3);
             s3.AddChild(s4);
             int a = 1;
             PerlinApp.Stage.EnterFrameEvent += (target, secs) =>
             {
-                // tf2.Width++;
                 s2.Rotation += 1f;
                 a++;
                 if (a > 99) a = 0;
                 // Console.WriteLine(s2.Rotation + " " + s2.GetBoundsWithChildren().Width);
             };
-            Button bb = new Button("Cancel");
-            bb.Y = 70;
-            bb.X = 5;
-            PerlinApp.Stage.AddChild(bb);
+            
+            PerlinApp.ShowStats(HorizontalAlignment.Right, VerticalAlignment.Bottom);
+        }
 
-            // drag test
-            var dragging = false;
-            Point dragOffset = new Point();
-            bb.MouseDown += (target, coords, button) =>
-            {
-                dragging = true;
-                dragOffset = bb.GlobalToLocal(coords);
-            };
-            bb.MouseMoved += (target, coords) =>
-            {
-                if (dragging)
-                {
-                    bb.X = coords.X - dragOffset.X;
-                    bb.Y = coords.Y - dragOffset.Y;
-                }
-            };
-            bb.MouseUp += (target, coords, button) => dragging = false;
-
-            // transparency test
+        private void TestTransparency()
+        {
             var t1 = new Sprite(10, 50, new Rgba32(123, 123, 123, 123)) { X = 10, Y = 200, Name = "t1" };
             var t2 = new Sprite(20, 50, new Rgba32(123, 123, 123, 123)) { X = 10, Y = 210, Name = "t2" };
             var t3 = new Sprite(30, 50, new Rgba32(123, 123, 123, 123)) { X = 10, Y = 220, Name = "t3" };
@@ -141,13 +104,40 @@ namespace Snake_Game
             transparencyBug2.Name = "topS";
             transparencyBug.AddChild(transparencyBug2);
             PerlinApp.Stage.AddChild(transparencyBug);
+        }
 
-            // test texture sub-region
+        private void TestTextureSubRegion()
+        {
             var subTex = new Sprite(
                     Path.Combine(AppContext.BaseDirectory, "Assets", "grizzly.png"), false,
                     new Rectangle(50, 50, 50, 50))
-            { X = 400, Y = 10 };
+                    { X = 400, Y = 10 };
             PerlinApp.Stage.AddChild(subTex);
+        }
+
+        private void TestDragging()
+        {
+            Button bb = new Button("Cancel");
+            bb.Y = 70;
+            bb.X = 5;
+            PerlinApp.Stage.AddChild(bb);
+            
+            var dragging = false;
+            Point dragOffset = new Point();
+            bb.MouseDown += (target, coords, button) =>
+            {
+                dragging = true;
+                dragOffset = bb.GlobalToLocal(coords);
+            };
+            bb.MouseMoved += (target, coords) =>
+            {
+                if (dragging)
+                {
+                    bb.X = coords.X - dragOffset.X;
+                    bb.Y = coords.Y - dragOffset.Y;
+                }
+            };
+            bb.MouseUp += (target, coords, button) => dragging = false;
         }
 
         private void TextFieldTests()
@@ -190,7 +180,7 @@ namespace Snake_Game
             };
             PerlinApp.Stage.AddChild(tf3);
             
-            var tf4 = new TextField(font, "ŐŰÍÉÁautosizedj")
+            var tf4 = new TextField(font, "Hello\n World!")
             {
                 BackgroundColor = Color.Azure,
                 X = 170,
@@ -198,6 +188,5 @@ namespace Snake_Game
             };
             PerlinApp.Stage.AddChild(tf4);
         }
-
     }
 }
